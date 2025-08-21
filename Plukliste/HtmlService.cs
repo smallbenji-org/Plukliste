6531eb4155c4b5cfa6e1829081308a64b6feb46b
HFGF <HFGF@DESKTOP-3N6HI60>
2025-08-21 13:18:53 +0200

    Added generative html files

--- Plukliste/HtmlService.cs
+++ Plukliste/HtmlService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,38 @@ namespace Plukliste
 {
     public class HtmlService
     {
+        public string? GenerateHTML(Pluklist pluklist, string templateFileName)
+        {
+            string templateData = "";
+
+            string dir = Path.Combine(Directory.GetCurrentDirectory(), "templates");
+
+            bool fileExists = Directory.GetFiles(dir).Any(file => file == Path.Combine(dir, templateFileName));
+
+            string templatePath = Path.Combine(dir, templateFileName);
+
+            if (!fileExists)
+            {
+                Console.WriteLine($"Template file \"{templateFileName}\" not found in templates directory.");
+                return null;
+            }
+
+            using (StreamReader sr = new StreamReader(templatePath))
+            {
+                templateData = sr.ReadToEnd();
+            }
+
+            templateData = templateData.Replace("[Name]", pluklist.Name);
+            templateData = templateData.Replace("[Adresse]", pluklist.Adresse);
+
+            foreach (var item in pluklist.Lines)
+            {
+                templateData = templateData.Replace("[Plukliste]", $"<tr><td>{item.Amount}</td><td>{item.Type}</td><td>{item.ProductID}</td><td>{item.Title}</td></tr>");
+            }
+
+            return templateData;
+        }
+
         public static void SaveHtmlFile(string content)
         {
             string[] temp = Directory.GetDirectories(Directory.GetCurrentDirectory());
@@ -23,7 +56,7 @@ namespace Plukliste
 
             string fileName = Guid.NewGuid().ToString();
             File.Create(Path.Combine(Directory.GetCurrentDirectory(), "print", $"{fileName}.html")).Close();
-            
+
             using (StreamWriter sw = new StreamWriter(Path.Combine(Directory.GetCurrentDirectory(), "print", $"{fileName}.html")))
             {
                 sw.WriteLine(content);

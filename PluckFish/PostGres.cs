5a0596e680c15e819f6620883413342df9c36e70
emilh <emilh@clausenwebworks.com>
2025-08-21 15:27:14 +0200

    .

---
+++ PluckFish/PostGres.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+using Dapper;
+using System.Data;
+
+namespace PluckFish
+{
+    public class PostGres
+    {
+        private readonly IConfiguration configuration;
+
+        public PostGres(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IDataReader ExecuteReader(string query)
+        {
+            using var conn = new NpgsqlConnection(configuration.GetConnectionString("defaultConnection"));           
+            return conn.ExecuteReader(query);
+        }
+        public object? ExecuteScalar(string query)
+        {
+            using var conn = new NpgsqlConnection(configuration.GetConnectionString("defaultConnection"));
+            return conn.Execute(query);
+        }
+    }
+}

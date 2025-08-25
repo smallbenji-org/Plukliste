4f2b6d932c1a35dcb84dea6f5d30d1023ce3220f
aqys <51910@edu.sde.dk>
2025-08-25 12:31:18 +0200

    Added ProductManager page, with functionality

---
+++ PluckFish/Controllers/ProductManagerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PluckFish.Interfaces;
+
+namespace PluckFish.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductManagerController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromForm] string prodName, [FromForm] string prodID)
+        {
+            productRepository.AddProduct(new Models.Product { Name = prodName, ProductID = prodID });
+
+            return RedirectToAction("Index");
+        }
+    }
+}

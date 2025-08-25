d1ed7b20d5ff2fab0769f926816ab4def560bae2
personal name <benjaminkfalch@gmail.com>
2025-08-25 14:03:21 +0200

    Fixing Thomas bug

--- PluckFish/Controllers/AuthController.cs
+++ PluckFish/Controllers/AuthController.cs
@@ -5,75 +5,85 @@ using PluckFish.Models;
 
 namespace PluckFish.Controllers
 {
+    [ApiController]
+    [Route("Auth")]
     public class AuthController : Controller
     {
-        [ApiController]
-        [Route("Auth")]
-        public class UserController : Controller
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AuthController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
-            private readonly SignInManager<ApplicationUser> signInManager;
-            private readonly UserManager<ApplicationUser> userManager;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
 
-            public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> IndexAsync([FromBody] LoginDto model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(model.username, model.password, isPersistent: true, lockoutOnFailure: false);
+
+            if (!result.Succeeded)
             {
-                this.signInManager = signInManager;
-                this.userManager = userManager;
+                return Unauthorized("Invalid login attempt.");
             }
+            return Ok("Logged in");
+        }
 
+        [HttpPost("FormLogin")]
+        public async Task<IActionResult> FormLogin([FromForm] string username, [FromForm] string password)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(username, password, isPersistent: true, lockoutOnFailure: false);
 
-            [HttpPost]
-            [Route("Login")]
-            public async Task<IActionResult> IndexAsync([FromBody] LoginDto model)
+            if (!result.Succeeded)
             {
-                Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(model.username, model.password, isPersistent: true, lockoutOnFailure: false);
-
-                if (!result.Succeeded)
-                {
-                    return Unauthorized("Invalid login attempt.");
-                }
-                return Ok("Logged in");
+                return Unauthorized("Invalid login attempt.");
             }
 
-            [HttpPost]
-            [Route("Register")]
-            public async Task<IActionResult> Register([FromBody]RegisterDto model)
+            return Ok("Logged in");
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+            ApplicationUser user = new ApplicationUser
             {
-                ApplicationUser user = new ApplicationUser
-                {
-                    UserName = model.username,
-                    FullName = model.fullname,
-                    Email = model.username
-                };
-                IdentityResult result = await userManager.CreateAsync(user, model.password);
+                UserName = model.username,
+                FullName = model.fullname,
+                Email = model.username
+            };
+            IdentityResult result = await userManager.CreateAsync(user, model.password);
 
-                if (!result.Succeeded)
-                {
-                    return BadRequest(result.Errors);
-                }
-
-                return Ok("User registered");
-            }
-
-            [HttpGet]
-            [Route("Logout")]
-            public async Task<IActionResult> Logout()
+            if (!result.Succeeded)
             {
-                await signInManager.SignOutAsync();
-                return Ok("Signed out");
+                return BadRequest(result.Errors);
             }
 
-            public class LoginDto
-            {
-                public string username { get; set; }
-                public string password { get; set; }
-            }
+            return Ok("User registered");
+        }
 
-            public class RegisterDto
-                            {
-                public string username { get; set; }
-                public string fullname { get; set; }
-                public string password { get; set; }
-            }
+        [HttpGet]
+        [Route("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return Ok("Signed out");
+        }
+
+        public class LoginDto
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+        public class RegisterDto
+        {
+            public string username { get; set; }
+            public string fullname { get; set; }
+            public string password { get; set; }
         }
     }
 }

d481e2d9450eb0cf371eba05c91f3568558b66f0
emilh <emilh@clausenwebworks.com>
2025-08-25 10:39:29 +0200

    CSV indlæsning af linjer færdig.

--- Plukliste/FileReader.cs
+++ Plukliste/FileReader.cs
@@ -1,5 +1,7 @@
 using Plukliste.Models;
 using System;
+using System.Data.Common;
+using System.Linq.Expressions;
 using System.Xml.Serialization;
 
 namespace Plukliste
@@ -7,16 +9,71 @@ namespace Plukliste
 {
     internal static class FileReader
     {
-        public static Pluklist ReadPluklist(string filePath)
+        private static Dictionary<string, int> getHeaderCSV(string headerRow)
         {
-            FileStream file = File.OpenRead(filePath);
+            Dictionary<string, int> newDict = new Dictionary<string, int>();
+            int indexCount = 0;
+            foreach (string column in headerRow.Split(';')) 
+            {             
+                newDict.Add(column.Replace("\r", ""), indexCount);
+                indexCount++;
+            }
+            return newDict;
+        }
+        public static Pluklist? ReadPluklist(string filePath)
+        {
+            filePath = "C:\\Users\\admin\\Desktop\\Skole\\638119645050937557_Kej_Nielsen.csv";
+            Pluklist? plukliste = new Pluklist();
+            if (filePath.EndsWith(".csv"))
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string[] rows = sr.ReadToEnd().Split('\n');
+                    if (rows.Length <= 0) return plukliste;
 
-            XmlSerializer xmlSerializer =
+                    Dictionary<string, int> headerDict = getHeaderCSV(rows[0]);
+                    foreach(string row in rows.Skip(1))
+                    {
+                        Item? item = new Item();
+                        string[] columns = row.Split(';');
+                        foreach (KeyValuePair<string, int> entry in headerDict)
+                        {
+                            if (columns.Count() < headerDict.Count()) continue;
+                            switch (entry.Key)
+                            {
+                                case "productid":
+                                    item.ProductID = columns[entry.Value];
+                                    break;
+                                case "type":
+                                    string type = columns[entry.Value];
+                                    if (type.ToUpper() == "FYSISK") 
+                                    {
+                                        item.Type = ItemType.Fysisk;
+                                    }else if (type.ToUpper() == "PRINT")
+                                    {
+                                        item.Type = ItemType.Print;
+                                    }
+                                    break;
+                                case "description":
+                                    item.ProductID = columns[entry.Value];
+                                    break;
+                                case "amount":
+                                    int.TryParse(columns[entry.Value], out item.Amount);
+                                    break;
+                            }
+                        }                        
+                        plukliste.AddItem(item);
+                    }
+                }
+            }
+            else            
+            {
+                FileStream file = File.OpenRead(filePath);
+                XmlSerializer xmlSerializer =
                 new XmlSerializer(typeof(Pluklist));
-
-            var plukliste = (Pluklist?)xmlSerializer.Deserialize(file);
-
-            file.Close();
+                plukliste = (Pluklist?)xmlSerializer.Deserialize(file);
+                file.Close();
+            }
 
             return plukliste;
         }

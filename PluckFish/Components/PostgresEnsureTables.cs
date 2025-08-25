43d23ce5780ed4145a034709a25fb02aff6debdb
Smallbenji <benjaminkfalch@gmail.com>
2025-08-25 12:12:02 +0200

    Adding base auth feature (#17)

--- PluckFish/Components/PostgresEnsureTables.cs
+++ PluckFish/Components/PostgresEnsureTables.cs
@@ -38,6 +38,22 @@ namespace PluckFish.Components
                     productId TEXT PRIMARY KEY,
                     name TEXT
                 );
+
+                CREATE TABLE IF NOT EXISTS roles (
+                    id TEXT PRIMARY KEY,
+                    name TEXT,
+                    normalized_name TEXT,
+                    description TEXT
+                );
+
+                CREATE TABLE IF NOT EXISTS users (
+                    id TEXT PRIMARY KEY,
+                    username TEXT,
+                    normalized_username TEXT,
+                    email TEXT,
+                    password_hash TEXT,
+                    full_name TEXT
+                );
             ");
         }
     }

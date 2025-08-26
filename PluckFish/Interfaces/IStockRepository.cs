a20a8177dd8407a25ddac2253f066fb9b089c960
emilh <emilh@clausenwebworks.com>
2025-08-26 12:24:58 +0200

    Code ready to be applied to the webpage.

---
+++ PluckFish/Interfaces/IStockRepository.cs
@@ -0,0 +1,11 @@
+﻿using PluckFish.Models;
+
+namespace PluckFish.Interfaces
+{
+    public interface IStockRepository
+    {
+        List<Item> getStock();
+        void saveStock(List<Item> savedStock);
+        void orderStock(List<Item> orderedStock);
+    }
+}

1a277fec37e70523e6d7e5f33f1247f6c0ef11bd
HFGF <HFGF@DESKTOP-3N6HI60>
2025-08-26 12:20:35 +0200

    added the ability to choose pickinglist

---
+++ PluckFish/Controllers/PickingListController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PluckFish.Interfaces;
+using PluckFish.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace PluckFish.Controllers
+{
+    public class PickingListController : Controller
+    {
+        private readonly ILogger<PickingListController> _logger;
+        private readonly IPickingListRepository pickingListRepository;
+        private readonly IProductRepository productRepository;
+
+        public PickingListController(ILogger<PickingListController> logger, IPickingListRepository pickingListRepository, IProductRepository productRepository)
+        {
+            _logger = logger;
+            this.pickingListRepository = pickingListRepository;
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var retval = new PickingListViewModel();
+
+            retval.PickingLists = pickingListRepository.GetAllPickingList();
+            return View(retval);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("PickingList/{Id}")]
+        public IActionResult GetPickingList(int Id)
+        {
+            var retval = new PickingListViewModel();
+            retval.PickingLists = pickingListRepository.GetAllPickingList();
+            retval.PickingListSelected = true;
+            retval.SelectedPickingList = pickingListRepository.GetPickingList(Id);
+
+            return View("Index", retval);
+        }
+
+        [Route("HandlePickingList")]
+        public IActionResult HandlePickingList([FromForm]string id)
+        {
+            return RedirectToAction(nameof(GetPickingList), new { Id = id });
+        }
+
+        public IActionResult Test()
+        {
+            List<PickingList> list = pickingListRepository.GetAllPickingList();
+            return Ok(list); // <-- Let ASP.NET handle serialization
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class PickingListViewModel
+    {
+        public List<PickingList> PickingLists { get; set; }
+        public bool PickingListSelected { get; set; } = false;
+        public PickingList SelectedPickingList { get; set; }
+    }
+}

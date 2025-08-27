88b9d410915dd69ad8b0ff6646bb0d30b801dc81
emilh <emilh@clausenwebworks.com>
2025-08-27 13:04:32 +0200

    Added getItemStock function.

--- PluckFish/Interfaces/IStockRepository.cs
+++ PluckFish/Interfaces/IStockRepository.cs
@@ -5,6 +5,7 @@ namespace PluckFish.Interfaces
     public interface IStockRepository
     {
         List<Item> getStock();
+        Item getItemStock(string prodId);
         bool stockExist(string prodId);
         void saveStock(Item savedStock);
         void orderStock(List<Item> orderedStock);

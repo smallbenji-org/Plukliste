665f682039b96737edd4a29e703beced5ea7e2d1
aqys <51910@edu.sde.dk>
2025-09-01 11:27:19 +0200

    Created bestillings page.

---
+++ PluckFish/Controllers/BestillingManagerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PluckFish.Interfaces;
+using PluckFish.ViewModels;
+using System.Linq;
+
+namespace PluckFish.Controllers
+{
+    public class BestillingManagerController : Controller
+    {
+        private readonly IStockRepository _stockRepository;
+
+        public BestillingManagerController(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var varer = _stockRepository.getStock().Take(15);
+
+            var viewModel = new BestillingManagerViewModel
+            {
+                Varer = varer
+            };
+
+            return View(viewModel);
+        }
+    }
+}

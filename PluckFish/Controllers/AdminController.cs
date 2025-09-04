5128cb9be5d12289808e6b1fff763dd96361769f
aqys <51910@edu.sde.dk>
2025-09-04 13:06:03 +0200

    Added new token page, and added styling.

---
+++ PluckFish/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PluckFish.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Tokenhåndtering";
+            return View();
+        }
+    }
+}
